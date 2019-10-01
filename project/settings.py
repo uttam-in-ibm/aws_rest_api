b238b869369a1c8cada665b0c368d37ea1691021
node <node>
2019-10-01 23:58:41 +0530

    correction in static

--- project/settings.py
+++ project/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = '/usr/local/apps/project/static/'
-STATIC_ROOT = '/usr/local/apps/project/static'
+STATICFILES_DIR = '/usr/local/apps/aws_rest_api/static/'
+STATIC_ROOT = '/usr/local/apps/aws_rest_api/static'
 
 
 
