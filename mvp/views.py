fe3467a9c62bdd5be5d79b70d5f842c2687cd261
node <node>
2019-10-01 23:33:31 +0530

    "Basic API"

---
+++ mvp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+
+class UserCreate(APIView):
+
+    def get(self, request, format=None):
+        content = {
+            "Message":"Hello"
+            }
+        return Response(content,status.HTTP_200_OK)
